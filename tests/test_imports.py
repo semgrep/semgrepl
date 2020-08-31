4fb927132e93a48335dd307e920fdb66fb6333a4
Daniel DeFreez <defreez@programanalys.is>
2020-08-31 09:19:10 -0700

    go import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,5 +1,10 @@
 import semgrepl.main as sm
 
+def test_go_import_simple():
+    config = sm.init("tests/testcases/go/imports/simple.go")
+    imports = sm.find_imports(config)
+    assert '"fmt"' in imports
+
 def test_python_import_simple():
     config = sm.init("tests/testcases/python/imports/simple.py")
     imports = sm.find_imports(config)
