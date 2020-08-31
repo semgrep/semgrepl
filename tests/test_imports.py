fa881f6a1cc885ceb028203038bf694506d5faac
Daniel DeFreez <defreez@programanalys.is>
2020-08-31 09:23:55 -0700

    java import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,7 +3,16 @@ import semgrepl.main as sm
 def test_go_import_simple():
     config = sm.init("tests/testcases/go/imports/simple.go")
     imports = sm.find_imports(config)
+
+    # Would be nice to strip quotes here.
     assert '"fmt"' in imports
+
+def test_java_import_simple():
+    config = sm.init("tests/testcases/java/imports/simple.java")
+    imports = sm.find_imports(config)
+
+    # Do we want / can we get the full path java.util.ArrayList?
+    assert "ArrayList" in imports
 
 def test_python_import_simple():
     config = sm.init("tests/testcases/python/imports/simple.py")
