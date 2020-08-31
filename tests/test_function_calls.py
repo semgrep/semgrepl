939b8ae896433aede6355a1674c8bac0cf36b3d7
Daniel DeFreez <defreez@programanalys.is>
2020-08-31 11:08:58 -0700

    cleanup

--- tests/test_function_calls.py
+++ tests/test_function_calls.py
@@ -9,8 +9,6 @@ def test_python_function_calls_simple():
 def test_python_function_calls_instance():
     config = sm.init("tests/testcases/python/function_calls/instance.py")
     calls = sm.all_function_calls(config)
-    import sys
-    print(calls, file=sys.stderr)
     assert len(calls) == 3
     for call in calls:
         if call.name == "write":
